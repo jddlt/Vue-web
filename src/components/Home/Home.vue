8c5f6c2a92eebb9bfa6cfb429d35b38fdcbb0184
jdd <2216775643@qq.com>
2019-09-03 23:47:13 +0800

    33333

--- src/components/Home/Home.vue
+++ src/components/Home/Home.vue
@@ -32,18 +32,94 @@
       </Menu>
       <div class="content fwb">
         <div class="left">
-          <div></div>
+          <div class="info">
+            <div class="info-head flex-cc">
+              <div class="info-img-box flex-cc">
+                <img src="./../../assets/img/user.jpg" alt="头像" class="info-img">
+              </div>
+            </div>
+            <div class="info-body flex-c-sc">
+              <div class="info-name c000 fwb fs20">{{userInfo.name}}</div>
+              <div class="info-mark c999 fwl fs16">该用户懒得写签名</div>
+            </div>
+            <div class="info-message flex-sc">
+              <div class="message-item flex-cc">
+                <i class="iconfont fs15 c999">&#xe61e;</i>
+                <span class="fs15 c999">2546</span>
+              </div>
+              <div class="message-item flex-cc">
+                <i class="iconfont fs14 c999">&#xe60c;</i>
+                <span class="fs15 c999">2546</span>
+              </div>
+              <div class="message-item flex-cc">
+                <i class="iconfont fs15 c999">&#xe6b0;</i>
+                <span class="fs15 c999">2546</span>
+              </div>
+            </div>
+            <div class="info-foot"></div>
+          </div>
+          <div class="block">
+
+          </div>
         </div>
         <div class="mid">
           <div class="head">
             今日话题
           </div>
-          <div class="list" v-for="(item, index) in wordList" :key="index">
+          <!-- <div class="list" v-for="(item, index) in wordList" :key="index">
 
+          </div> -->
+          <div class="list" v-for="num in 6" :key='num'>
+            <div class="list-head flex-sc">
+              <div class="list-head-left flex-sc">
+                <img src="./../../assets/img/user.jpg" alt="头像" class="list-img">
+                <div class="list-name flex-c-s">
+                  <span class="fs14 c333">{{userInfo.name}}</span>
+                  <span class="lh100">
+                    <i class="iconfont fs11">&#xe629;</i>
+                    <span class="c999 fs11 lh100">10分钟以前</span>
+                  </span>
+                </div>
+              </div>
+              <i class="list-like iconfont fs22 cccc flex-cc">&#xe61d;</i>
+            </div>
+            <div class="list-body">
+              <p class="list-title fs16 c333 fwb">Senior Wordpress Developer</p>
+              <p class="list-content fs15 c666 fwl">疏星淡月，紫陌曲岸，持觞游赏，神移长川。一片彀纹，溶溶泄泄，忽而烟靡云敛。睹一丽人，缦立青水，云蒸雾霭，花衬善睐。荧荧兮若北辰之荣现，扰扰兮若紫玉之生烟。颜如舜华，迫闻素腰华琚摇;和颜静志，远望渌水呈雾绡。戏流光之夜蝶，采舞雪之琼花，流眷眷之眸光，润荣曜之笑靥。</p>
+            </div>
+            <div class="list-foot flex-sc">
+              <div class="foot-left flex-sc">
+                <span class="list-message flex-cc">
+                  <i class="iconfont fs15 c999">&#xe60c;</i>
+                  <span class="fs15 c999">2546</span>
+                </span>
+                <span class="list-message flex-cc">
+                  <i class="iconfont fs15 c999">&#xe61a;</i>
+                  <span class="fs15 c999">2546</span>
+                </span>
+              </div>
+              <div class="list-message flex-cc">
+                <i class="iconfont fs20 c999">&#xe633;</i>
+                <span class="fs15 c999">2546</span>
+              </div>
+            </div>
           </div>
         </div>
         <div class="right">
-          
+          <div class="send">
+            <div class="send-head cfff fs18 fwb flex-cc">发帖</div>
+            <div class="send-content">
+              <input type="text" class="input fs15" placeholder="请输入您的标题">
+              <textarea class="textarea fs15" cols="30" rows="10" placeholder="请输入您的内容"></textarea>
+              <!-- <input type="textarea" class="textarea" placeholder="请输入您的内容"> -->
+              <div class="btn fs16 cfff flex-cc fwn">
+                发布
+              </div>
+            </div>
+          </div>
+          <div class="block">
+
+          </div>
         </div>
       </div>
       <div class="footer">
@@ -111,7 +187,8 @@ export default {
 .home{
   width: 100%;
   height: 100%;
-  background: url('./../../assets/img/bgc.jpg') no-repeat bottom fixed;
+  background: #ccc url('./../../assets/img/bgc.jpg') no-repeat bottom fixed;
+  // background: #ccc;
   background-size: cover;
   overflow: auto;
   zoom: 1;
@@ -162,45 +239,231 @@ export default {
 }
 .content{
   width: 1200px;
-  height: 1400px;
-  background-color: #fff;
+  // height: 1400px;
+  // background-color: #ccc;
   margin: 100px auto;
   margin-bottom: 60px;
   color: #4158D0;
-  font-size: 35px;
   display: flex;
   justify-content: space-between;
   align-items: flex-start;
   .left{
     width: 23%;
     height: 100%;
-    background-color: #ccc;
+    // background-color: #ccc;
+    .info{
+      width: 100%;
+      // height: 400px;
+      background-color: #fff;
+      .info-head{
+        width: 100%;
+        height: 100px;
+        position: relative;
+        background-color: #4158D0;
+        .info-img-box{
+          width: 100px;
+          height: 100px;
+          border-radius: 50%;
+          position: absolute;
+          bottom: -50px;
+          background-color: #fff;
+          .info-img{
+            width: 93px;
+            height: 93px;
+            border-radius: 50%;
+            transition: transform 1s;
+            &:hover{
+              transform: rotate(360deg)
+            }
+          }
+        }
+      }
+      .info-body{
+        width: 100%;
+        height: 120px;
+        padding: 30px 12px;
+        box-sizing: border-box;
+        border-bottom: 1px solid #ccc;
+        margin-top: 50px;
+        // .info-name{}
+      }
+      .info-message{
+        width: 100%;
+        height: 60px;
+        padding: 0 30px;
+        box-sizing: border-box;
+        .message-item{
+          height: 100%;
+          cursor: pointer;
+          transition: all 0.3s;
+          &:hover>i,&:hover>span{
+            color: #4158D0 !important;
+            font-size: 16px;
+          }
+        }
+      }
+      .info-foot{
+        width: 100%;
+        height: 20px;
+        background-color: #4158D0;
+      }
+    }
   }
   .mid{
     width: 50%;
     height: 100%;
-    background-color: #aaa;
+    // background-color: #ccc;
     .head{
       width: 100%;
-      height: 80px;
+      height: 70px;
       box-shadow: 0 0 2px #ccc;
       border-top: 5px solid #4158D0;
-      background-color: #ccc;
+      background-color: #fff;
       text-align: center;
-      line-height: 80px;
+      line-height: 70px;
       padding: 0 30px;
       box-sizing: border-box;
       letter-spacing: 40px;
+      font-size: 30px;
     }
     .list{
       width: 100%;
       height: auto;
+      background-color: #fff;
+      padding: 0 25px;
+      box-sizing: border-box;
+      margin-top: 20px;
+      .list-head{
+        width: 100%;
+        height: 60px;
+        .list-head-left{
+          width: 120px;
+          height: 100%;
+          .list-img{
+            width: 35px;
+            height: 35px;
+            border-radius: 50%;
+            margin-right: 8px;
+          }
+          .list-name{
+            height: 30px;
+          }
+        }
+        .list-like{
+          width: 32px;
+          height: 100%;
+          cursor: pointer;
+          &:hover{
+            animation: bigger 0.5s linear;
+            animation-fill-mode: forwards;
+          }
+        }
+        @keyframes bigger {
+          25% {
+            font-size: 32px;
+            color: rgba(255, 0, 0, 0.205)
+          }
+          50% {
+            font-size: 24px;
+            color: rgba(255, 0, 0, 0.479)
+          }
+          75% {
+            font-size: 28px;
+            color: rgba(255, 0, 0, 0.733)
+          }
+          100%{
+            font-size: 24px;
+            color: rgb(255, 0, 0)
+          }
+        }
+      }
+      .list-body{
+        width: 100%;
+        height: auto;
+        border-bottom: 1px solid #ccc;
+        .list-title{
+          width: 100%;
+          // height: 30px;
+          line-height: 30px;
+        }
+        .list-content{
+          width: 100%;
+          // height: 60px;
+          padding: 8px 0 15px 0;
+          text-indent: 2em;
+          line-height: 150%;
+        }
+      }
+      .list-foot{
+        width: 100%;
+        height: 40px;
+        .foot-left{
+          width: 120px;
+        }
+        .list-message{
+          height: 100%;
+          &:hover>i,&:hover>span{
+            color: #4158D0 !important;
+            font-size: 16px;
+          }
+        }
+      }
     }
   }
   .right{
     width: 23%;
     height: 100%;
-    background-color: #ccc;
+    // background-color: #ccc;
+    .send{
+      width: 100%;
+      height: auto;
+      background-color: #fff;
+      .send-head{
+        width: 100%;
+        height: 50px;
+        background-color: #4158D0;
+      }
+      .send-content{
+        width: 100%;
+        padding: 12px;
+        box-sizing: border-box;
+        .input{
+          width: 100%;
+          height: 30px;
+          border: none;
+          outline: none;
+          background-color: #ccc;
+          border-radius: 3px;
+          margin-top: 12px;
+          padding-left: 5px;
+          box-sizing: border-box;
+        }
+        .textarea{
+          text-indent: 1em;
+          width: 100%;
+          height: 160px;
+          border: none;
+          outline: none;
+          background-color: #ccc;
+          border-radius: 5px;
+          margin-top: 12px;
+          padding: 4px;
+          box-sizing: border-box;
+          line-height: 120%;
+        }
+        .btn{
+          width: 100%;
+          height: 30px;
+          border-radius: 4px;
+          background-color: #4158D0;
+          margin-top: 12px;
+          cursor: pointer;
+          &:active{
+            background: linear-gradient(to right top , #4158D0, #C850C0);
+          }
+        }
+      }
+    }
   }
 }
 .footer{
@@ -209,6 +472,15 @@ export default {
   text-align: center;
   line-height: 50px;
   font-size: 18px;
+  background-color: #fff;
+}
+i{
+  margin-right: 3px;
+}
+.block{
+  width: 100%;
+  height: 300px;
+  margin-top: 20px;
   background-color: #fff;
 }
 </style>
